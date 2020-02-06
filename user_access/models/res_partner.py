55d7c0cef82bc3613a5211caa13e26d6962be4ca
Dharmik Ponda <dharmikp@emiprotechnologies.com>
2020-02-06 17:58:35 +0530

    [add] update user role in partner model

--- user_access/models/res_partner.py
+++ user_access/models/res_partner.py
@@ -3,10 +3,16 @@
 import logging
 import uuid
 
-from odoo import models, fields
+from odoo import api, fields, models
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    user_role = fields.Selection([('l1', "L1 User"), ('l2', "L2 User")],related='user_ids.user_role')
+    user_role = fields.Selection([('l1', "L1 User"), ('l2', "L2 User")],related='user_ids.user_role',readonly=False)
+
+    @api.onchange('user_role')
+    def update_user_role(self):
+        if self.user_role and self.parent_id.user_id:
+            self.parent_id.user_id.user_role = self.user_role
+
 
